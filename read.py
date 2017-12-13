939a9c646c9ad89fdaaa7458b9ea6fd980e59772
Ethan <lyflyflxf@gmail.com>
2017-12-13 11:15:06 +0800

    localize to Sur

--- read.py
+++ read.py
@@ -12,7 +12,10 @@ unit='Unit'
 english='单词'
 
 if __name__=="__main__":
-    xls = pd.ExcelFile(files['21'])
+    if input('选择：1.杨瀚森 2.尹嘉禾')=='2':
+        xls = pd.ExcelFile(files['21'])
+    else:
+        xls = pd.ExcelFile(files['high'])
     s=xls.parse('Sheet1')
     units=s.loc[:, unit]
     words=list(s[units==1][english])
@@ -22,7 +25,7 @@ if __name__=="__main__":
     rate = engine.getProperty('rate')
     engine.setProperty('rate', rate - 70)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[2].id)
+    engine.setProperty('voice', voices[1].id)
 
     for i, word in enumerate(words):
         def this():
@@ -30,11 +33,13 @@ if __name__=="__main__":
             engine.runAndWait()
         while 1:
             os.system('cls')
-            print('第', i + 1, '个单词，共', len(words), '个。\n回车：下一词  其他键+回车：重新播放； ')
+            print('第', i + 1, '个单词，共', len(words), '个。\n回车：下一词  其他键+回车：重新播放；')# 1+回车：退出
             this()
             type_in=input()
             if type_in=='':
                 break
+            #elif type_in=='1':
+            #    os.exit()
             else:
                 continue#print('输入错误，请重新输入')
         # if type_in==
